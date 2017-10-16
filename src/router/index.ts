3b44dfd6b1275ba8972fd3b279a76307ed126bae
BcRikko <s.shin1106@gmail.com>
2017-10-16 09:37:42 +0900

    :shirt: tslintの導入

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Vote from '@/pages/Vote/vote.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Vote from '@/pages/Vote/vote.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -12,4 +12,4 @@ export default new Router({
       component: Vote
     }
   ]
-})
+});
